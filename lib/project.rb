04c0a2317adc35a246b35872f7cdbffddb7a82fc
github username <github email address>
2019-01-31 10:47:58 -0800

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,15 @@
+class Project
+  attr_accessor :backers
+  attr_reader :title
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self
+    # self refering to the instance this method is acting on
+  end
+end
