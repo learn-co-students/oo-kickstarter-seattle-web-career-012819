d6ecfec2ce6f3911ca3f3097a80654288bfcc4ea
github username <github email address>
2019-01-31 10:53:17 -0800

    day2 paired programming

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,11 +1,12 @@
 class Backer
-  attr_accessor :backed_projects, :name, :project
+  attr_accessor :backed_projects, :name
   def initialize(name)
     @backed_projects=[]
     @name=name
   end
   def back_project(project)
     @backed_projects << project
+    project.add_backer(self)
 
   end
 
