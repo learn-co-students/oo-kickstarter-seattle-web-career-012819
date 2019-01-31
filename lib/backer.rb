450b4cb830bf1d29a28d70392bce5de1e7bed023
omfgtora <spaminol@gmail.com>
2019-01-31 10:33:15 -0800

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,20 @@
+class Backer
+    attr_reader :name
+
+    @@backed_projects = []
+
+    def initialize(name)
+        @name = name
+    end
+
+    def back_project(project)
+        @@backed_projects << project
+        if !(project.backers.include?(self))
+            project.add_backer(self)
+        end
+    end
+
+    def backed_projects
+        @@backed_projects
+    end
+end
